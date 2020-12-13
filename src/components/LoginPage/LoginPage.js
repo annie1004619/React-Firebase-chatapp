8fb58de8692fe60cf67db0fead917e34374fb460
annie1004619 <annie1004619@naver.com>
2020-12-14 03:41:47 +0900

    [ADD] loginpage firebase와 연결

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import firebase from "../../firebase";
 
 const LoginPage = () => {
-  return <div>login</div>;
+  const { register, errors, handleSubmit } = useForm({
+    mode: "onChange",
+  });
+  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      setErrorFromSubmit(error.message);
+      setLoading(false);
+      setTimeout(() => {
+        setErrorFromSubmit("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Login</h1>
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && errors.email.type === "required" && (
+          <p>이메일은 반드시 입력해야합니다.</p>
+        )}
+        {errors.email && errors.email.type === "pattern" && (
+          <p>이메일이 형식에 맞지 않습니다.</p>
+        )}
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          ref={register({ required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>비밀번호는 반드시 입력해야합니다.</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>비밀번호는 6글자 이상이여야 합니다.</p>
+        )}
+
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
+        <input type="submit" value="submit" disabled={loading} />
+        <Link to="/register" style={{ color: "gray", textDecoration: "none" }}>
+          아직 아이디가 없다면...
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
