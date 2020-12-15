d934f01332acc81e53b663fc1363d6d9273de948
annie1004619 <annie1004619@naver.com>
2020-12-15 23:53:59 +0900

    [ADD]ChatPage MainPanel MessageHeader UI

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MessageHeader = () => {
-  return <div>messageheader</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "170px",
+        border: ".2rem solid #ececec",
+        borderRadius: "4px",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    ></div>
+  );
 };
 
 export default MessageHeader;
